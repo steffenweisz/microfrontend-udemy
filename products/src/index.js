a1d86983d7ec1376b2475b9b89d98d63a9e87b80
Steffen Weisz <138871714+steffenweisz@users.noreply.github.com>
2024-07-30 22:39:29 +0200

    🚧 change index.js from products

--- products/src/index.js
+++ products/src/index.js
@@ -2,7 +2,7 @@ import faker from 'faker';
 
 let products = "";
 
-for (let i = 0; i < 6; i++) {
+for (let i = 0; i < 10; i++) {
   const name = faker.commerce.productName();
   products += `<div>${name}</div>`;
 }
